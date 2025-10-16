7a690472c2e15cb4c131902493e859e164a8d583
Chandan Biswas <pela.chandan@gmail.com>
2025-10-16 00:28:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from utils.email_utils import send_email_alert
 
 if __name__ == "__main__":
     print("🚀 Running SMA crossover and 52-week high scan...")
-    sma_list, high_list = run_scan()
+    sma_list, high_list = run_scan(test_mode=True)
 
     print("\n📈 SMA Signals:")
     for s in sma_list:
